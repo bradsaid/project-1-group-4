7f778adea3688f968ad19fd460cd197804c506a9
Brad Said <bradsaid@gmail.com>
2018-06-19 20:56:30 -0500

    Image js correction

--- assets/javascript/images.js
+++ assets/javascript/images.js
@@ -2,7 +2,8 @@ $("#add-destination").on("click", function(event) {
     event.preventDefault();
     $("#images-view").empty();
     var API_KEY = '9335517-b03c3ef104729c3975acac324';
-    let image = $("#destinaton-input").val().trim();
+    let image = $("#destination-input").val().trim();
+    console.log(image)
     var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(image);
     $.getJSON(URL, function(data){
         for (var i = 0; i < data.hits.length; i++) {
