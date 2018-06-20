7352b667db93444de89783ad71592d691310387b
bqtphan <bqtphan@utexas.edu>
2018-06-19 21:30:42 -0500

    added video to html

--- assets/javascript/video.js
+++ assets/javascript/video.js
@@ -1,16 +1,17 @@
 // YOUTUBE API
-$("#add-destination").on("click", function(event) {
+$("#add-destination").on("click", function (event) {
     event.preventDefault();
-    var searchterm = $("#destination-input").val().trim();
+    var searchterm = $("#destination-input").val().trim() + " vacation";
     console.log(searchterm);
     var APIkey = "&key=AIzaSyD_xUzbIohfKHueav2f1FBmmq5DVo7KE-A";
     var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + searchterm + APIkey;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-      console.log(response)
-
-  });
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        var videoID = response.items[0].id.videoId;
+        console.log(videoID);
+    }); 
 });
