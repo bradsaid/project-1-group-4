b840d08140a34c435e22f93c269625a6c2750b71
bqtphan <bqtphan@utexas.edu>
2018-06-21 20:21:53 -0500

    Make sure it is merged into master

--- assets/javascript/video.js
+++ assets/javascript/video.js
@@ -15,6 +15,6 @@ $("#add-destination").on("click", function (event) {
         console.log(videoID);
         var videoURL = "https://www.youtube.com/embed/" + videoID +"?autoplay=1";
         console.log(videoURL)
-        $("#video").append('<iframe width="420" height="315" src="' + videoURL + '"></iframe>');
+        $("#video").html('<iframe width="420" height="315" src="' + videoURL + '"></iframe>');
     }); 
 });
