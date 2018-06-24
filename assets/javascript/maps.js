41f164c06967c553cfd497caef75eba2ac800876
Alex Federovitch <alexfederovitch@yahoo.com>
2018-06-24 14:32:25 -0500

    adding updated maps.js, index.htnml, and style.css files with map style

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -1,51 +1,11 @@
-// let map, infoWindow;
-// let myposition;
-// function initAutocomplete() {
-//   let map = new google.maps.Map(document.getElementById('map-view'), {
-//     center: {lat: 29.760426700000004, lng: -95.3698028},
-//     zoom: 12,
-//     mapTypeId: 'roadmap'
-//   });
-//   infoWindow = new google.maps.InfoWindow;
-
-
-// if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(function(position) {
-//     let pos = {
-//       lat: position.coords.latitude,
-//       lng: position.coords.longitude
-//     };
-//     console.log(pos);
-//     myposition=pos;
-//     infoWindow.setPosition(pos);
-//     infoWindow.setContent('You\'re here.');
-//     infoWindow.open(map);
-//     map.setCenter(pos);
-//   }, function() {
-//     handleLocationError(true, infoWindow, map.getCenter());
-//   });
-// } else {
-//   // Browser doesn't support Geolocation
-//   handleLocationError(false, infoWindow, map.getCenter());
-// }
-
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-// infoWindow.setPosition(pos);
-// infoWindow.setContent(browserHasGeolocation ?
-//                       'Error: The Geolocation service failed.' :
-//                       'Error: Your browser doesn\'t support geolocation.');
-// infoWindow.open(map);
-// }
-
 
 function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map-view'), {
       center: {lat: -33.8688, lng: 151.2195},
-      zoom: 15,
+      zoom: 13,
       mapTypeId: 'roadmap'
     });
 
-    console.log('hello');
 
     // Create the search box and link it to the UI element.
     var input = document.getElementById('destination-input');
@@ -57,6 +17,7 @@ function initAutocomplete() {
     map.addListener('bounds_changed', function() {
       searchBox.setBounds(map.getBounds());
     });
+
 
     var markers = [];
     // Listen for the event fired when the user selects a prediction and retrieve
@@ -106,4 +67,4 @@ function initAutocomplete() {
       });
       map.fitBounds(bounds);
     });
-  }
+}
