2f9774a349c5bdbb61c11ddf65d7933529af8e59
Brad Said <bradsaid@gmail.com>
2018-06-26 10:45:54 -0500

    Working on images

--- assets/javascript/images.js
+++ assets/javascript/images.js
@@ -1,9 +1,17 @@
+
+function hasImages() {
+    if ($("#images-view:has(.images)"))  {
+        console.log("has images");
+    } else {
+        console.log("No pictures");
+    }
+};
+
 $("#add-destination").on("click", function(event) {
     event.preventDefault();
     $("#images-view").empty();
     var API_KEY = '9335517-b03c3ef104729c3975acac324';
     let image = $("#destination-input").val().trim();
-    console.log(image)
     var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(image)
     + "&per_page=15"
     + "&category=travel"
@@ -11,8 +19,14 @@ $("#add-destination").on("click", function(event) {
     $.getJSON(URL, function(data){
         for (var i = 0; i < data.hits.length; i++) {
             let url = data.hits[i].webformatURL;
-            let id = "images"
-            $("#images-view").append("<img src=" + url + " id=" + id + ">");
+            let id = "images";
+            let c = "images";
+            $("#images-view").append("<img src=" + url + " id=" + id + " "+ "class=" + c + ">");
         };
+    $("#images-view").empty();
+    hasImages();
 });
+
+
 });
+
